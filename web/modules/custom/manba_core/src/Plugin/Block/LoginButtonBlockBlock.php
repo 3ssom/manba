3a2a1a2b348d1be02abbf7fbe02aca8c35e246a2
Mohammed Bamlhes <bamlhes@sharedtech.com.sa>
2022-11-18 22:58:07 +0300

    Add new custom block for login button

---
+++ web/modules/custom/manba_core/src/Plugin/Block/LoginButtonBlockBlock.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\manba_core\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+
+/**
+ * Provides a loginblock block.
+ *
+ * @Block(
+ *   id = "login_button_block",
+ *   admin_label = @Translation("Login Button Block"),
+ *   category = @Translation("Custom")
+ * )
+ */
+class LoginButtonBlockBlock extends BlockBase
+{
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build()
+  {
+
+    $build = [];
+    $currentUser = \Drupal::currentUser();
+    if (\Drupal::currentUser()->isAnonymous()) {
+      $url = Url::fromRoute('user.login');
+      $link = new Link(t('Log In'), $url);
+      $renderable = $link->toRenderable();
+      $build['link'] = $renderable;
+      $build['link']['#attributes'] = ['class' => ['btn_get btn_hover saas_btn hidden-sm hidden-xs']];
+    }
+
+
+    return $build;
+  }
+}
