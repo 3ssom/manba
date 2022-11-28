3eabd650492460caa479aecc4400a31755472934
Mohammed Bamlhes <bamlhes@gmail.com>
2022-11-29 00:21:17 +0300

    Close #46 , Search block finalize

--- web/modules/custom/manba_core/src/Plugin/Block/SearchBlock.php
+++ web/modules/custom/manba_core/src/Plugin/Block/SearchBlock.php
@@ -19,13 +19,31 @@ class SearchBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
+    $language_id =  \Drupal::languageManager()->getCurrentLanguage()->getId();
+    $terms = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadByProperties([
+      'vid' => 'keywords',
+      'langcode' => $language_id
+    ]);
+    $results = [];
+    $suggestion = [];
+    foreach ($terms as $term) {
+      if ($term->get('field_suggestion')->value) {
+        $suggestion[$term->id()] = $term->getName();
+      }
+      $results[$term->id()] = $term->getName();
+    }
     return array(
       '#theme' => 'search_block',
       '#attached' => array(
         'library' => array(
           'custom_theme/tagify',
         ),
+        'drupalSettings' => array(
+          'suggestion' => array_values($suggestion),
+          'results' => array_values($results),
+        ),
       ),
+      '#cache' => ['tags' => ['taxonomy_term_list:keywords']],
     );
   }
 
--- web/themes/custom/custom_theme/js/custom-tagify.js
+++ web/themes/custom/custom_theme/js/custom-tagify.js
@@ -4,21 +4,7 @@ jQuery(function ($) {
     var input = document.querySelector("input[name=tags-outside]");
     // init Tagify script on the above inputs
     var tagify = new Tagify(input, {
-      whitelist: [
-        "Drupal",
-        "Wordpress",
-        "Laravel",
-        "PHP",
-        "CMS",
-        "Linux",
-        "Git",
-        "Joomla",
-        "Angular",
-        "React",
-        "Vue",
-        "Node",
-        "Express",
-      ],
+      whitelist: drupalSettings.results,
       maxTags: 4,
       dropdown: {
         position: "input",
@@ -27,28 +13,21 @@ jQuery(function ($) {
       },
     });
 
-    $(".search-btn").on("click", function () {
+    $(".search-btn").on("click", function (e) {
+      e.preventDefault();
       var tags = tagify.getCleanValue();
-      console.log(tags);
+      const keywords = [];
+      for (i = 0; i < tags.length; ++i) {
+        keywords.push(tags[i].value);
+      }
+      if (keywords.length > 0) {
+        console.log(keywords);
+        window.location = "search/node?keys=" + keywords.join(" ");
+      }
     });
   }
-
   var typed = new Typed(".elm-typed", {
-    strings: [
-      "Drupal",
-      "Wordpress",
-      "Laravel",
-      "PHP",
-      "CMS",
-      "Linux",
-      "Git",
-      "Joomla",
-      "Angular",
-      "React",
-      "Vue",
-      "Node",
-      "Express",
-    ],
+    strings: drupalSettings.suggestion,
     typeSpeed: 80,
     backSpeed: 40,
     backDelay: 500,
